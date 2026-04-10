3e98ae0ad7d0d0d494bce84118ef978b24b92a61
Ned Basilio <nedbasilio@gmail.com>
2026-04-10 19:57:52 +0800

    Made all the functions before this commit dynamic. Program now asks the user for the row and columns. Program is now dynamic meaning we can make maps/matrices custom sizes.

--- gridMovementProject.py
+++ gridMovementProject.py
@@ -5,14 +5,14 @@ def createMap(row, column):
     grid_map = [[0 for _ in range(column)] for _ in range(row)]
     return grid_map
 
-def setOuterLimit(givenRow, givenColumn):
-    for row in range(givenRow):
-        for column in range(givenColumn):
-            if (row == 0 or row == givenRow-1):
-                grid_map[row][column] = 2
+def setOuterLimit():
+    for i in range(row):
+        for j in range(column):
+            if (i == 0 or i == row-1):
+                grid_map[i][j] = 2
 
-            if (column == 0 or column == givenColumn-1):
-                grid_map[row][column] = 2
+            if (j == 0 or j == column-1):
+                grid_map[i][j] = 2
     return grid_map
 
 def showMap():
@@ -21,14 +21,26 @@ def showMap():
             print(element, end=" ")
         print("")
 
+def setPlayerLocation():
+    grid_map[row//2][column//2] = 1
+
+    return grid_map
+
 def getPlayerLocation(): # returns 2 values: row, column
-    for row in range(5):
-        for column in range(5):
-            if grid_map[row][column] == 1:
-                return row, column
+    playerFound = False
+    for i in range(row):
+        for j in range(column):
+            if grid_map[i][j] == 1:
+                playerFound = True
+                foundRow, foundColumn = i, j
+    
+    if (playerFound):
+        return foundRow, foundColumn
+    else:
+        return -1, -1
             
 def isLocationValid(playerRow, playerCol):
-    if (((playerRow == 0) or (playerRow == 4)) or ((playerCol == 0) or (playerCol == 4))):
+    if (((playerRow == 0) or (playerRow == row-1)) or ((playerCol == 0) or (playerCol == column-1))):
         return False
     else:
         return True
@@ -51,14 +63,25 @@ def moveLeft(lastRow, lastCol):
 
 # main program below
 
-row = int(input("What will be the number of rows for this map: "))
-column = int(input("What will be the number of columns for this map: "))
+while True:
+    row = int(input("What will be the number of rows for this map: "))
+    column = int(input("What will be the number of columns for this map: "))
+
+    if ((row >= 2) and (column >= 2)):
+        break
+    elif (row < 3):
+        print("Error! row must be greater than 2.")
+    elif (column < 3):
+        print("Error! column must be greater than 2.")
 
 grid_map = createMap(row, column)
-grid_map = setOuterLimit(row, column)
+grid_map = setOuterLimit()
+grid_map = setPlayerLocation()
 showMap()
 
-'''
+playerRow, playerColumn = getPlayerLocation()
+print(f"Player is at [{playerRow}][{playerColumn}]")
+
 newRow, newCol = getPlayerLocation()
 
 
@@ -78,10 +101,12 @@ while (isLocationValid(newRow, newCol)):
         case _:
             print("Error, invalid movement input.")
 
-    showMap()
+    
     newRow, newCol = getPlayerLocation()
     if (isLocationValid(newRow, newCol)):
+        showMap()
         print("Player is still alive")
     else:
+        grid_map[newRow][newCol] = 2
+        showMap()
         print("Player hit the grid. Player has died.")
-'''
