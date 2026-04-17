96b153769956addb845496015828708498e50f15
Ned Basilio <nedbasilio@gmail.com>
2026-04-17 21:01:10 +0800

    Removed get_player_location(), replaced with get_index_of_integer for a more dynamic function.

--- location_tools.py
+++ location_tools.py
@@ -4,7 +4,7 @@ location_tools.py
 This module has setplayerlocation and getplayerlocation methods.
 """
 
-def set_player_location(grid_map):
+def set_player_location_middle(grid_map):
     """
         Sets the player location on the middle of the grid_map
 
@@ -20,24 +20,17 @@ def set_player_location(grid_map):
 
     return grid_map
 
-def get_player_location(grid_map): # returns 2 values: row, column
-    """
-        Loops through grid_map to find where the player (1) is located
+def get_index_of_integer(grid_map, given_integer):
+    """This function takes in grid_map (2d List) and given_integer as its parameters.
+    returns row, column of where the given_integer is."""
 
-        Args:
-            grid_map (2d List): grid_map it will scan
-
-        Returns:
-            found_row (int): row index of player location
-            found_column (int): row column of player location
-    """
     row = len(grid_map)
     column = len (grid_map[0])
     found_row, found_column = -1, -1
 
     for i in range(row):
         for j in range(column):
-            if grid_map[i][j] == 1:
+            if grid_map[i][j] == given_integer:
                 found_row, found_column = i, j
     
     return found_row, found_column
