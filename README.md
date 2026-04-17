81318ba8fb91524bf0b49d1a09fa7f94cbfee32b
Ned Basilio <nedbasilio@gmail.com>
2026-04-17 20:49:17 +0800

    Updated main and readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 This small project basically uses a 2d list to to show the playable area. This must not have any good reasons or causes but I wanna try making a small program that takes in my current coding knowledge from my first year of CS and basically the progress of my learnings.
 
 roomTraversal branch is for improving most of locationTools and to make Room Traversal possible with generateMap(). In the previous commits of this program, the code checks if the player has 'stepped' on the outer limits of the grid_map. Now this branch will implement that it will just check if the player stepped on 2 in the 2d list.
+
+Ideas:
+    Dynamic Map Shapes
+        this will make each grid_map dynamic with different or combined shapes
+        currently dont have any idea how can I do this, will need research
+
+    Randomly Generated Structures/Objects
+        this will generate random integers on the 0s, these integers will represent objects that the player will need
+        or something
+
+    Goal/Enter Room Condition
+        Maybe set a random location or somewhere in the outer limits to maybe number 4. 4 basically means if the player
+        manages to reach this index, they will advance to another room.
+        (This project is gonna need more handling on more grid_maps)
--- main.py
+++ main.py
@@ -1,46 +1,20 @@
-import keyboard
+"""main.py is the main python file"""
+
 import time
 import os
-import mapTools
-import locationTools
-import playerTools
+import keyboard
+import map_tools
+import location_tools
+import player_tools
 # grid_map is the map of the level or stage
 # TO DO: Try to learn about OOP or refactor the entire program before developing next ideas
-"""
-Ideas:
-    Dynamic Map Shapes
-        this will make each grid_map dynamic with different or combined shapes
-        currently dont have any idea how can I do this, will need research
 
-    Randomly Generated Structures/Objects
-        this will generate random integers on the 0s, these integers will represent objects that the player will need
-        or something
 
-    Goal/Enter Room Condition
-        Maybe set a random location or somewhere in the outer limits to maybe number 4. 4 basically means if the player
-        manages to reach this index, they will advance to another room.
-        (This project is gonna need more handling on more grid_maps)
-"""
-
-"""
-while True:
-    This was before adding generateMap()
-    row = int(input("What will be the number of rows for this map: "))
-    column = int(input("What will be the number of columns for this map: "))
-
-    if ((row >= 2) and (column >= 2)):
-        break
-    elif (row < 3):
-        print("Error! row must be greater than 2.")
-    elif (column < 3):
-        print("Error! column must be greater than 2.")
-    """
-
-def determineEvent(indexValue):
+def determine_event(index_value):
     """
         Determines what event depending on the integer passed onto this function.
     """
-    eventIntegers = {
+    event_integers = {
         0: "FREE",
         1: "PLAYER",
         2: "DEATH",
@@ -48,72 +22,72 @@ def determineEvent(indexValue):
         4: "NEW_ROOM"
     }
 
-    if (indexValue) in eventIntegers:
-        print(eventIntegers[indexValue])
-        return eventIntegers[indexValue]
+    if index_value in event_integers:
+        print(event_integers[index_value])
+        return event_integers[index_value]
     else:
-        return "Unregistered or invalid event: {}".format(indexValue)
+        return str(f"Unregistered or invalid event: {index_value}")
 
-def applyEvent(event):
+def apply_event(given_event):
     """
-        Applies the event, complements determineEvent(). This function needs to return True if player is alive.
+    Applies the event, complements determine_event(). This function needs to return True if player is is_alive.
     """
     global grid_map
-    if (event == "DEATH"):
+    if given_event == "DEATH":
         return False
-    elif (event == "NEW_ROOM"):
-        grid_map = mapTools.generateMap()
-        grid_map = locationTools.setPlayerLocation(grid_map)
-        return True
 
+    if given_event == "NEW_ROOM":
+        grid_map = map_tools.generate_map()
+        grid_map = location_tools.set_player_location(grid_map)
+        return True
 
     return True
 
-#grid_map = mapTools.createMap(row, column)
-grid_map = mapTools.generateMap()
-grid_map = locationTools.setPlayerLocation(grid_map)
-mapTools.showMap(grid_map)
+#grid_map = map_tools.create_map(row, column)
+grid_map = map_tools.generate_map()
+grid_map = location_tools.set_player_location(grid_map)
+map_tools.show_map(grid_map)
 
-playerRow, playerColumn = locationTools.getPlayerLocation(grid_map)
-print(f"Player is at [{playerRow}][{playerColumn}]")
+player_row, player_column = location_tools.get_player_location(grid_map)
+print(f"Player is at [{player_row}][{player_column}]")
 
 key_states = {"w": False, "a": False, "s": False, "d": False}
-alive = True
+is_alive = True
 event = ""
 
-while alive:
-    lastRow, lastCol = locationTools.getPlayerLocation(grid_map)
-    #print(f"Last Row: {lastRow}, Last Column: {lastCol}")
+while is_alive:
+    last_row, last_col = location_tools.get_player_location(grid_map)
+    #print(f"Last Row: {last_row}, Last Column: {last_col}")
 
-    #alive = applyEvent(event)
+    #is_alive = apply_event(event)
 
     moved = False
-    for key in key_states:
+    for key, _ in key_states.items():
         if keyboard.is_pressed(key):
             if not key_states[key]:
                 try:
-                    event = determineEvent(playerTools.checkNextIndex(grid_map, key))
+                    event = determine_event(player_tools.check_next_index(grid_map, key))
                     match key:
-                        case 'w': playerTools.up(grid_map)
-                        case 's': playerTools.down(grid_map)
-                        case 'a': playerTools.left(grid_map)
-                        case 'd': playerTools.right(grid_map)
+                        case 'w': player_tools.up(grid_map)
+                        case 's': player_tools.down(grid_map)
+                        case 'a': player_tools.left(grid_map)
+                        case 'd': player_tools.right(grid_map)
                 except IndexError as ie:
                     print("Error index invalid.")
 
                 moved = True
                 key_states[key] = True
-                alive = applyEvent(event)
+                is_alive = apply_event(event)
                 break
         else:
             key_states[key] = False
 
     if moved:
         os.system('cls' if os.name == 'nt' else 'clear')
-        mapTools.showMap(grid_map)
-        playerRow, playerColumn = locationTools.getPlayerLocation(grid_map)
-        print(f"Player is at [{playerRow}][{playerColumn}]")
-        #newRow, newCol = getPlayerLocation()
+        map_tools.show_map(grid_map)
+        player_row, player_column = location_tools.get_player_location(grid_map)
+        print(f"Player is at [{player_row}][{player_column}]")
+        #new_row, new_col = getPlayerLocation()
 
     if keyboard.is_pressed("esc"):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -122,8 +96,8 @@ while alive:
 
     time.sleep(0.05)
 
-lastRow, lastCol = locationTools.getPlayerLocation(grid_map)
+last_row, last_col = location_tools.get_player_location(grid_map)
 os.system('cls' if os.name == 'nt' else 'clear')
-grid_map[lastRow][lastCol] = 3
-mapTools.showMap(grid_map)
+grid_map[last_row][last_col] = 3
+map_tools.show_map(grid_map)
 print("Player hit the grid. Player has died")
