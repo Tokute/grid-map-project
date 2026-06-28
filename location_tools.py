92042b01956d99b9fc7d020c7494659db922bd3f
Tokute <nedbasilio@gmail.com>
2026-06-28 13:45:30 +0800

    adjusted docstring by js a bit

--- location_tools.py
+++ location_tools.py
@@ -5,8 +5,8 @@ This module has and get_index_of_integer methods.
 """
 
 def get_index(grid_map, given_obj):
-    """This function takes in grid_map (2d List) and given_integer as its parameters.
-    returns row, column of where the given_integer is."""
+    """This function takes in grid_map (obj) and given_obj (obj) as its parameters.
+    returns row, column of where the given_obj is, returns (-1, -1) (int) if not found."""
 
     row = len(grid_map.get_map())
     column = len(grid_map.get_map()[0])
